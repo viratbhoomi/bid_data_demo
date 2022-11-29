eade35af28e051a28862d06789660bc1366f65ef
viratbhoomi <viratvits@gmail.com>
2022-11-29 11:12:24 +0530

    prime numbers

--- test_1.py
+++ test_1.py
@@ -19,7 +19,26 @@ class demo_class_2(demo_class_1):
     def display_2(self):
         return self.name, self.age, self.city, self.country, self.status, self.employment, self.salary
 
+class prime_numbers():
+    def __init__(self,num):
+        self.num = num
+
+    def display_3(self):
+        if self.num>1:
+            for i in range(2,self.num):
+                if (self.num%i)==0:
+                    print(self.num, " is not a prime number")
+                    break
+            else:
+                print(self.num, " is a prime number")
+        else:
+            print(self.num, " is not a prime number")     
+
+                
+
 
 x = demo_class_2("Vikram",45,"Chennai","India","Single","IT",35000)
 print(x.display_2())
+y = prime_numbers(3)
+y.display_3()
     
